74d025213ad68ea7f47cae8b9b31b0d21a5af325
Serg_Grish <balchyg@icloud.com>
2021-08-13 22:10:26 +0300

    5

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const _ = require('lodash');
 
-import fetchCountries from './fetchCountries.js';
-import '../node_modules/@pnotify/core/dist/Angeler.css';
-import '../node_modules/@pnotify/core/dist/BrightTheme.css';
-import '../node_modules/@pnotify/core/dist/Material.css';
-import '../node_modules/@pnotify/core/dist/PNotify.css';
-import './style.css';
+import fetchCountries from './fetchCountries.js'
+import '../node_modules/@pnotify/core/dist/Angeler.css'
+import '../node_modules/@pnotify/core/dist/BrightTheme.css'
+import '../node_modules/@pnotify/core/dist/Material.css'
+import '../node_modules/@pnotify/core/dist/PNotify.css'
+import './styles.css';
+
 
 let searchQuery = '';
 const URL = `https://restcountries.eu/rest/v2/name/`;
 const input = document.querySelector('.finder');
 
-input.addEventListener(
-  'input',
-  _.debounce(event => {
-    fetchCountries(event, searchQuery, URL);
-  }),
-  500,
-);
+input.addEventListener('input', _.debounce((event) => {
+    fetchCountries(event,searchQuery,URL)
+}),500)
+
+
+
