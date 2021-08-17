4fdfb5c2154a52d96be7daab703f276ec287b2fc
Serg_Grish <balchyg@icloud.com>
2021-08-17 23:48:43 +0300

    2 master

--- src/index.js
+++ src/index.js
@@ -1,20 +1,2 @@
-const _ = require('lodash');
-
-import fetchCountries from './fetchCountries.js'
-import '../node_modules/@pnotify/core/dist/Angeler.css'
-import '../node_modules/@pnotify/core/dist/BrightTheme.css'
-import '../node_modules/@pnotify/core/dist/Material.css'
-import '../node_modules/@pnotify/core/dist/PNotify.css'
 import './styles.css';
-
-
-let searchQuery = '';
-const URL = `https://restcountries.eu/rest/v2/name/`;
-const input = document.querySelector('.finder');
-
-input.addEventListener('input', _.debounce((event) => {
-    fetchCountries(event,searchQuery,URL)
-}),500)
-
-
-
+import './js/search.js';
---
+++ src/js/getRefs.js
@@ -0,0 +1,6 @@
+export default function getRefs() {
+  return {
+    form: document.querySelector('.jsForm'),
+    countryCard: document.querySelector('.countriesList'),
+  };
+}
